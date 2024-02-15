994302a92732555e66aaf2fd16d24fc91000e35b
Hicaru <hicc0rus@gmail.com>
2024-02-15 13:46:37 +0700

    added depends

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,86 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useBearby } from '@hicaru/bearby-react';
+import { web3 } from '@hicaru/bearby.js';
+
+import {
+  Address,
+  changeCallStack,
+  resetStorage,
+  setDeployContext,
+} from '@massalabs/massa-as-sdk';
+import {
+  Args,
+  stringToBytes,
+  u8toByte,
+  bytesToU256,
+  u256ToBytes,
+} from '@massalabs/as-types';
+import {
+  transfer,
+  balanceOf,
+  totalSupply,
+  name,
+  symbol,
+  decimals,
+  version,
+  transferFrom,
+  allowance,
+  increaseAllowance,
+  decreaseAllowance,
+  constructor,
+  VERSION,
+} from '../../massa-standards/smart-contracts/assembly/contracts/FT/token';
+import { u256 } from 'as-bignum/assembly';
 
 function App() {
+  const { connected, enabled, wallet, massa, contract, base58, net, period } = useBearby();
+
+  React.useEffect(() => {
+    console.log("works");
+
+  }, []);
+
+  const deploy = React.useCallback(() => {
+    const TOKEN_NAME = 'TOKEN_NAME';
+    const TOKEN_SYMBOL = 'TKN';
+    const DECIMALS: u8 = 8;
+    const TOTAL_SUPPLY = new u256(100, 100, 100, 100);
+
+    constructor(
+      new Args()
+        .add(TOKEN_NAME)
+        .add(TOKEN_SYMBOL)
+        .add(DECIMALS)
+        .add(TOTAL_SUPPLY)
+        .serialize(),
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        connected:({connected})
+      </p>
+      <p>
+        period:({period})
+      </p>
+      <p>
+        enabled:({enabled})
+      </p>
+      <p>
+        base58:({base58})
+      </p>
+      <p>
+        network:({net})
+      </p>
+      <button onClick={() => wallet.connect()}>
+        connect
+      </button>
+      <button onClick={() => deploy()}>
+        deploy FT
+      </button>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BearbyProvider } from '@hicaru/bearby-react';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  (globalThis as any).document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BearbyProvider>
+      <App />
+    </BearbyProvider>
   </React.StrictMode>
 );
 
